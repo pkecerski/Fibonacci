4a66556ec61e38c327d98cfae43f38aee671a8a3
Paweł Kęcerski <pkecerski@icloud.com>
2021-03-16 16:54:00 +0100

    Version 0.1

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,1 +1,37 @@
+//  fibonacci
+//  Created by Paweł Kęcerski on 2021/03/16
+//  @pkecerski
+//  Prints elements of the Fibonacci Sequence up to the n-th element
 
+
+#include <iostream>
+#include <stdio.h>
+
+//  Define namespace for easier typing
+using namespace std;
+
+//  Define functions
+void fibonacci();
+
+// Define variables
+
+
+// Program loop
+int main(int argc, char *argv[]){
+        fibonacci();
+}
+
+// Custom functions
+void fibonacci(){
+    int nElements;
+    double dPi = 3.14159, dPiApprox;
+
+    //  Prompt user for desired number of elements
+    printf("Please enter how many elements to print: \n");
+    scanf("%d",&nElements);
+
+    //  Fibonacci Sequence
+    for (int i=0; i<(nElements); i++){
+       
+    }
+}
